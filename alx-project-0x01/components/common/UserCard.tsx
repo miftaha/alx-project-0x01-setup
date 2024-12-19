31c29e615b678b6550e80af7f714499e5663e1b1
Miftah <miftahaadem@gmail.com>
2024-12-19 19:14:08 +0300

    Implement Users Card Components

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,44 @@
+import { UserProps } from '@/interfaces'
+const UserCard: React.FC<UserProps> = ({
+  name,
+  username,
+  email,
+  address,
+  phone,
+
+  company,
+}) => {
+  return (
+    <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg  hover:shadow-xl transition-shadow duration-300">
+      <div className="p-6">
+        <h2 className="text-lg font-bold mb-2">{name}</h2>
+        <p className="text-sm text-gray-500 mb-4">@{username}</p>
+
+        <div className="text-sm">
+          <p className="font-semibold">Email:</p>
+          <p>{email}</p>
+        </div>
+
+        <div className="text-sm mt-4">
+          <p className="font-semibold">Address:</p>
+          <p>
+            {address.suite}, {address.street}, {address.city}, {address.zipcode}
+          </p>
+        </div>
+
+        <div className="text-sm mt-4">
+          <p className="font-semibold">Phone:</p>
+          <p>{phone}</p>
+        </div>
+
+        <div className="text-sm mt-4">
+          <p className="font-semibold">Company:</p>
+          <p>{company.name}</p>
+          <p className="italic">"{company.catchPhrase}"</p>
+          <p>{company.bs}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default UserCard
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,5 +1,42 @@
-const Users: React.FC = () => {
-  return <div className="p-4">Users Page</div>
+import Header from '@/components/layout/Header'
+import UserCard from '@/components/common/UserCard'
+import { UserProps } from '@/interfaces'
+
+interface UsersPageProps {
+  posts: UserProps[]
+}
+
+const Users: React.FC<UsersPageProps> = ({ posts }) => {
+  console.log(posts)
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className=" text-2xl font-semibold">User Content</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+            Add User
+          </button>
+        </div>
+        <div className="grid grid-cols-3 gap-2 ">
+          {posts?.map((post) => (
+            <UserCard key={post.id} {...post} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
 
 export default Users
